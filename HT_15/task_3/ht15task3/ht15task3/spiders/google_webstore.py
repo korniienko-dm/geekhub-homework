b70f0aec3c5d31e57b052219dd1b2ba8e7fc6c12
dm_aspire <dm.aspire@mail.com>
2023-12-10 22:53:41 +0200

    HT_15

--- HT_15/task_3/ht15task3/ht15task3/spiders/google_webstore.py
+++ HT_15/task_3/ht15task3/ht15task3/spiders/google_webstore.py
@@ -35,38 +35,3 @@ class GoogleWebstoreSpider(scrapy.Spider):
             'name_extension': name_extension,
             'short_description': short_description
         }
-
-
-
-
-
-
-
-# scrapy crawl google_webstore -o google_extension_info.csv
-
-# class GoogleWebstoreSpider(scrapy.Spider):
-#     name = "google_webstore"
-#     allowed_domains = ["chrome.google.com"]
-#     start_urls = ['https://chrome.google.com/webstore/sitemap']
-
-#     def parse(self, response):
-#         for url_tag in response.css('sitemap loc'):
-#             result = url_tag.get()
-#             yield scrapy.Request(url=result, callback=self.parse_extension_url)
-
-#     def parse_extension_url(self, response):
-#         for url_extention in response.css('url loc'):
-#             result = url_extention.get()
-#             yield scrapy.Request(url=result, callback=self.parse_page_extension)
-
-#     def parse_page_extension(self, response):
-#         url_extension = response.url
-#         id_extension = url_extension.split('/')[-1]
-#         name_extension = response.css('h1.Pa2dE::text').get()
-#         short_description = response.css('.uORbKe p::text').get()
-
-#         yield {
-#             'id_extension': id_extension,
-#             'name_extension': name_extension,
-#             'short_description': short_description
-#         }
